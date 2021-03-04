88ceb61a9e12293ca121e9e73e69cacf5a6755bc
Aliaksandr Yanushkevich <aliaksandr.yanushkevich@innowise-group.com>
2021-03-04 17:13:53 +0300

    feat: add observer pattern

---
+++ patterns/observer.js
@@ -0,0 +1,27 @@
+class Observer {
+    constructor() {
+        this.subscribers = []
+    }
+
+    subscribe(fn) { // passed function in subscribe method. this function will execute with data from broadcast
+        this.subscribers.push(fn)
+    }
+
+    unsubscribe(fn) { // remove function from subscribers array
+        this.subscribers = this.subscribers.filter(subscriber => subscriber !== fn)
+    }
+
+    broadcast(data) {
+        this.subscribers.forEach(subscriber => subscriber(data));
+    }
+}
+
+const textArea = document.querySelector('#text')
+const counter = document.querySelector('#counter')
+
+const wordObserver = new Observer
+wordObserver.subscribe((value) => counter.innerHTML =value.length)
+textArea.addEventListener('keyup', () => wordObserver.broadcast(textArea.value));
+
+
+
